961b5364894c01e051331677c770c0d7585fda76
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-07-04 13:17:56 +0300

    complete labs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskInput = document.getElementById('new-task-description');
+    const taskDescription = taskInput.value;
+
+    addTask(taskDescription);
+
+    taskInput.value = '';
+  });
 });
+
+function addTask(taskDescription) {
+  const taskItem = document.createElement('li');
+  taskItem.textContent = taskDescription;
+
+  const taskList = document.getElementById('tasks');
+  taskList.appendChild(taskItem);
+}
